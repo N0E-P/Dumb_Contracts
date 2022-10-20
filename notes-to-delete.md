59f571890ce587e78e277f2cbb3b7f4078a185af
N0E-P <pecci.noe@gmail.com>
2022-10-20 12:44:37 +0200

    hola !

--- notes-to-delete.md
+++ notes-to-delete.md
@@ -1,3 +1,36 @@
 first remix account : 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
 second remix account : 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
 0.01 ETH (collateral) : 10000000000000000
+
+---
+
+intégration frontend
+
+create a contract
+
+delete hardhat repo ? just leave the dapp with the smart contract inside ?
+
+my contracts
+
+website style
+
+---
+
+prix auquels je pourrais prétendre :
+chainlink quality winners (doit utiliser chainlink)
+filecoin / ipfs (doit faire un truc en rapport)
+polygon (il suffit de deployer dessus)
+quicknode([https://www.quicknode.com/](https://www.quicknode.com/))
+
+Visu du contract ?
+nft
+image sur ipfs
+filecoin (permet de chopper des chances de gagner en plus)
+
+gestion d’un cas de dispute
+DAO en cas de litige :
+et une partie du collateral sera reversé aux personnes qui votent
+dans ce cas, les deux participants pourraient s’exprimer et donner leurs avis sur le probleme
+les votants choissisent combien d’argent va à qui.
+Ils voient le texte du contract en cours
+va changer moneyForUser1 & 2
--- pages/create.js
+++ pages/create.js
@@ -1,45 +1,45 @@
-import Head from "next/head";
-import Link from "next/link";
+import Head from "next/head"
+import Link from "next/link"
 
 export default function Create() {
-  return (
-    <div>
-      <Head>
-        <title>Create a Contract</title>
-        <meta name="description" content="Create a contract" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>Create a Contract</h1>
-      <div className="p-4 flex">
-        <Link href="/">
-          <button>Home</button>
-        </Link>
-        <div className="mx-4">
-          <Link href="/create">
-            <button>Create a contract</button>
-          </Link>
-        </div>
-        <div className="mx-4">
-          <Link href="/contracts">
-            <button>Your contracts</button>
-          </Link>
-        </div>
-      </div>
-      <div className="p-4">
-        <h3>Formulaire à remplir</h3>
-        <div>Nom du contrat</div>
-        <div>Texte expliquant le contract que l’on veut créer</div>
-        <div>Addresse ou ENS name de la 2nd personne</div>
-        <div>Collateral mis par chacunes des 2 personnes</div>
-        <div>Une fois le contract validé :</div>
-        <div className="px-3">vous récupérez chacuns vos collatéraux</div>
-        <div className="px-3">? montant que vous donner a personne 2</div>
-        <div className="px-3">? montant que personne 2 vous donnera</div>
-        <div className="px-3">
-          (l’argent donné et le collateral doit être payé sur le smart contract
-          dès sa création)
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<Head>
+				<title>Create a Contract</title>
+				<meta name="description" content="Create a contract" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<h1>Create a Contract</h1>
+			<div className="p-4 flex">
+				<Link href="/">
+					<button>Home</button>
+				</Link>
+				<div className="mx-4">
+					<Link href="/create">
+						<button>Create a contract</button>
+					</Link>
+				</div>
+				<div className="mx-4">
+					<Link href="/contracts">
+						<button>Your contracts</button>
+					</Link>
+				</div>
+			</div>
+			<div className="p-4">
+				<h3>Formulaire à remplir</h3>
+				<div>Nom du contrat</div>
+				<div>Texte expliquant le contract que l’on veut créer</div>
+				<div>Addresse ou ENS name de la 2nd personne</div>
+				<div>Collateral mis par chacunes des 2 personnes</div>
+				<div>Une fois le contract validé :</div>
+				<div className="px-3">vous récupérez chacuns vos collatéraux</div>
+				<div className="px-3">? montant que vous donner a personne 2</div>
+				<div className="px-3">? montant que personne 2 vous donnera</div>
+				<div className="px-3">
+					(l’argent donné et le collateral doit être payé sur le smart contract dès sa
+					création)
+				</div>
+			</div>
+		</div>
+	)
 }
